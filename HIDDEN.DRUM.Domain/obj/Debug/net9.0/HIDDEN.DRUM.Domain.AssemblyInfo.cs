467c88592e40ea642498c8b858d8bdf6485214ed
Maverick Walker <mtwbusiness102@outlook.com>
2025-03-26 15:43:12 -0400

    V6

--- HIDDEN.DRUM.Domain/obj/Debug/net9.0/HIDDEN.DRUM.Domain.AssemblyInfo.cs
+++ HIDDEN.DRUM.Domain/obj/Debug/net9.0/HIDDEN.DRUM.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HIDDEN.DRUM.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07c5105214a0a73b192d4ba0d596ac9869ed14ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("HIDDEN.DRUM.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HIDDEN.DRUM.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
