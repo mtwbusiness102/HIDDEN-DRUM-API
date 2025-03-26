7613e017b85bc144e135206b89ea69b69e7d7ba1
Maverick Walker <mtwbusiness102@outlook.com>
2025-03-26 15:37:01 -0400

    V4

---
+++ HIDDEN.DRUM.Domain/Catalog/item.cs
@@ -0,0 +1,14 @@
+using System; 
+
+namespace HIDDEN.DRUM.Domain.Catalog 
+{ 
+    public class Item 
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+        public decimal Price { get; set; } 
+    }
+
+}
